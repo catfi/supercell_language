5ea7d632cddbc9c056a8af084f24e0e5dec45fd9
Jerry <jerry@zillians.com>
2011-07-19 11:57:47 +0800

    fix bug in thor test

--- test/testzillians-compiler/ThorScriptParserTest/fundamentals/test.sh
+++ test/testzillians-compiler/ThorScriptParserTest/fundamentals/test.sh
@@ -6,8 +6,12 @@ COUNTER=-1
 for ARG in "$@"; do
     if [ $COUNTER -gt -1 ]; then
         echo "now testing file #$COUNTER.."
-        echo "$ARG"
-        $EXEC $ARG
+        if [ -f "$ARG" ]; then
+            echo "$ARG"
+            $EXEC $ARG
+        else
+            echo "ERROR: file not found!"
+        fi
         echo
     fi
     let COUNTER=$COUNTER+1;
