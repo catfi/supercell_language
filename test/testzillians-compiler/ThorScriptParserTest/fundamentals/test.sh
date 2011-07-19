b0cefaae123f382cbe95451ba22d21394782ab5d
Jerry <jerry@zillians.com>
2011-07-19 11:32:42 +0800

    fix bug in test.sh

--- test/testzillians-compiler/ThorScriptParserTest/fundamentals/test.sh
+++ test/testzillians-compiler/ThorScriptParserTest/fundamentals/test.sh
@@ -7,7 +7,7 @@ for ARG in "$@"; do
     if [ $COUNTER -gt -1 ]; then
         echo "now testing file #$COUNTER.."
         echo "$ARG"
-        $EXEC $COUNTER
+        $EXEC $ARG
         echo
     fi
     let COUNTER=$COUNTER+1;
