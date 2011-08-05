e06f35de7d4dbb1659fd46cb49658bcd1e7ff976
Mark Sung <mark@zillians.com>
2011-08-05 19:18:56 +0800

    add ThorScript AST draft

---
+++ test/testzillians-compiler/ThorScriptTreeTest/BasicTreeGenerationTest/BasicTreeGenerationTest.cpp
@@ -0,0 +1,42 @@
+/**
+ * Zillians MMO
+ * Copyright (C) 2007-2010 Zillians.com, Inc.
+ * For more information see http://www.zillians.com
+ *
+ * Zillians MMO is the library and runtime for massive multiplayer online game
+ * development in utility computing model, which runs as a service for every
+ * developer to build their virtual world running on our GPU-assisted machines.
+ *
+ * This is a close source library intended to be used solely within Zillians.com
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @date Sep 1, 2009 sdk - Initial version created.
+ */
+
+#include "core/Prerequisite.h"
+#include "compiler/tree/ASTNode.h"
+#include "compiler/tree/ASTNodeFactory.h"
+#include <iostream>
+#include <string>
+#include <limits>
+
+#define BOOST_TEST_MODULE ThorScriptTreeTest_BasicTreeGenerationTest
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+using namespace zillians;
+
+BOOST_AUTO_TEST_SUITE( ThorScriptTreeTest_BasicTreeGenerationTestSuite )
+
+BOOST_AUTO_TEST_CASE( ThorScriptTreeTest_BasicTreeGenerationTestCase1 )
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
