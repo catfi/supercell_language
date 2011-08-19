63390b07b88113dc8ffaedbd8f978996cf047767
Mark Sung <mark@zillians.com>
2011-08-19 18:10:59 +0800

    fix compilation error for ThorScriptTreeTest_BasicTreeGenerationTest

--- test/testzillians-compiler/ThorScriptTreeTest/BasicTreeGenerationTest/BasicTreeGenerationTest.cpp
+++ test/testzillians-compiler/ThorScriptTreeTest/BasicTreeGenerationTest/BasicTreeGenerationTest.cpp
@@ -44,7 +44,7 @@ BOOST_AUTO_TEST_CASE( ThorScriptTreeTest_BasicTreeGenerationTestCase1_IsA )
 	}
 
 	{
-		ASTNode *node = new BinaryExpr(BinaryExpr::OpCode::ASSIGN, new SimpleIdentifier(L"abc"), new NumericLiteral((uint64)123L));
+		ASTNode *node = new BinaryExpr(BinaryExpr::OpCode::ASSIGN, new PrimaryExpr(new SimpleIdentifier(L"abc")), new PrimaryExpr(new NumericLiteral((uint64)123L)));
 		BOOST_CHECK(isa<Expression>(node));
 		BOOST_CHECK(isa<BinaryExpr>(node));
 	}
