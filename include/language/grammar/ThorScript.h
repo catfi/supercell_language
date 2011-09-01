b93e2799d6b57672b81e7d78b35fe0da333ca992
Jerry <jerry@zillians.com>
2011-09-01 16:49:14 +0800

    refactor thor SA -- cleanup redundancy

--- include/language/grammar/ThorScript.h
+++ include/language/grammar/ThorScript.h
@@ -699,9 +699,9 @@ struct ThorScript : qi::grammar<Iterator, typename SA::start::attribute_type, de
 			;
 
 		branch_statement
-			=	( (RETURN > -expression > SEMICOLON) [ typename SA::branch_statement::init_return() ]
-				| (BREAK > SEMICOLON)                [ typename SA::branch_statement::template init<tree::BranchStmt::OpCode::BREAK>() ]
-				| (CONTINUE > SEMICOLON)             [ typename SA::branch_statement::template init<tree::BranchStmt::OpCode::CONTINUE>() ]
+			=	( (RETURN > expression_statement) [ typename SA::branch_statement::init_return() ]
+				| (BREAK > SEMICOLON)             [ typename SA::branch_statement::template init<tree::BranchStmt::OpCode::BREAK>() ]
+				| (CONTINUE > SEMICOLON)          [ typename SA::branch_statement::template init<tree::BranchStmt::OpCode::CONTINUE>() ]
 				)
 			;
 
