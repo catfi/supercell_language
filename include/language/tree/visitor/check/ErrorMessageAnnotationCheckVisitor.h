58963c81c2c1517c556e14d210315f62b4e53769
yoco <yoco@zillizns.com>
2011-09-08 20:53:07 +0800

    Check static-test/semantic-error annotation struct with the ErrorInfoContext

---
+++ include/language/tree/visitor/check/ErrorMessageAnnotationCheckVisitor.h
@@ -0,0 +1,136 @@
+/**
+ * Zillians MMO
+ * Copyright (C) 2007-2010 Zillians.com, Inc.
+ * For more information see http://www.zillians.com
+ *
+ * Zillians MMO is the library and runtime for massive multiplayer online game
+ * development in utility computing model, which runs as a service for every
+ * developer to build their virtual world running on our GPU-assisted machines.
+ *
+ * This is a close source library intended to be used solely within Zillians.com
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @date Aug 10, 2011 sdk - Initial version created.
+ */
+
+#ifndef ZILLIANS_LANGUAGE_TREE_VISITOR_ERRORMESSAGEANNOTATIONCHECKVISITOR_H_
+#define ZILLIANS_LANGUAGE_TREE_VISITOR_ERRORMESSAGEANNOTATIONCHECKVISITOR_H_
+
+#include <string>
+#include "core/Prerequisite.h"
+#include "core/Visitor.h"
+#include "language/tree/ASTNodeFactory.h"
+#include "language/tree/visitor/general/GenericDoubleVisitor.h"
+#include "language/stage/parser/context/SourceInfoContext.h"
+#include "language/context/StaticTestContext.h"
+
+namespace zillians { namespace language { namespace tree { namespace visitor {
+
+struct ErrorMessageAnnotationCheckVisitor : public GenericDoubleVisitor
+{
+	CREATE_INVOKER(errorMessageAnnotationCheckInvoker, check);
+
+	ErrorMessageAnnotationCheckVisitor() : mAllMatch(true)
+	{
+		REGISTER_ALL_VISITABLE_ASTNODE(errorMessageAnnotationCheckInvoker)
+	}
+
+	bool isAllMatch() const
+	{
+		return mAllMatch;
+	}
+
+	void check(ASTNode& node)
+	{
+		revisit(node);
+	}
+
+	void check(Annotation& node)
+	{
+		using zillians::language::stage::ErrorInfoContext ;
+		if (node.name->name == L"static_test")
+		{
+			// get error info context on node
+			// the parent of Annotation is Annotations,
+			// the error info context is hooked on the parent of Annotations.
+			ErrorInfoContext* errorInfo = ErrorInfoContext::get(node.parent/*Annotations*/->parent);
+			if(errorInfo == NULL)
+			{
+				mAllMatch = false;
+				return;
+			}
+			ErrorInfoContext constructedErrorInfo = constructErrorContextFromAnnotation(node);
+			if(*errorInfo != constructedErrorInfo)
+			{
+				mAllMatch = false;
+				return;
+			}
+		}
+	}
+
+private:
+	zillians::language::stage::ErrorInfoContext constructErrorContextFromAnnotation(Annotation& node)
+	{
+		using zillians::language::tree::cast;
+		//zillians::language::stage::ErrorInfoContext result;
+		BOOST_ASSERT(node.attribute_list.size() == 1);
+		// expect-message
+		std::pair<SimpleIdentifier*, ASTNode*>& expectMessage = node.attribute_list[0];
+		//BOOST_ASSERT(expectMessage != NULL);
+		BOOST_ASSERT(cast<SimpleIdentifier>(expectMessage.first)->name == L"expect_message");
+		BOOST_ASSERT(cast<Annotation>(expectMessage.second) != NULL);
+		BOOST_ASSERT(cast<Annotation>(expectMessage.second)->attribute_list.size() == 3);
+
+		// log level
+		std::pair<SimpleIdentifier*, ASTNode*> logLevel = cast<Annotation>(expectMessage.second)->attribute_list[0];
+		BOOST_ASSERT(cast<SimpleIdentifier>(logLevel.first) != NULL);
+		BOOST_ASSERT(cast<SimpleIdentifier>(logLevel.first)->name == L"level");
+		BOOST_ASSERT(cast<StringLiteral>(logLevel.second) != NULL);
+		std::wstring levelString = cast<StringLiteral>(logLevel.second)->value;
+
+		// log id
+		std::pair<SimpleIdentifier*, ASTNode*> logId = cast<Annotation>(expectMessage.second)->attribute_list[1];
+		BOOST_ASSERT(cast<SimpleIdentifier>(logId.first) != NULL);
+		BOOST_ASSERT(cast<SimpleIdentifier>(logId.first)->name == L"id");
+		BOOST_ASSERT(cast<StringLiteral>(logId.second) != NULL);
+		std::wstring idString = cast<StringLiteral>(logId.second)->value;
+
+		// parameter pairs
+		std::pair<SimpleIdentifier*, ASTNode*> paramPairs = cast<Annotation>(expectMessage.second)->attribute_list[2];
+		BOOST_ASSERT(cast<SimpleIdentifier>(paramPairs.first) != NULL);
+		BOOST_ASSERT(cast<SimpleIdentifier>(paramPairs.first)->name == L"parameters");
+		BOOST_ASSERT(cast<Annotation>(paramPairs.second) != NULL);
+		Annotation* params = cast<Annotation>(paramPairs.second);
+		std::map<std::wstring, std::wstring> paramsResult;
+		foreach(i, params->attribute_list)
+		{
+			SimpleIdentifier *paramKey = cast<SimpleIdentifier>(i->first);
+			BOOST_ASSERT(paramKey != NULL);
+			StringLiteral *paramValue = cast<StringLiteral>(i->second);
+			BOOST_ASSERT(paramValue != NULL);
+			std::wstring key = paramKey->name;
+			std::wstring value = paramValue->value;
+			//zillians::language::stage::ErrorInfoContext::parameter_type_t param(key, value);
+			std::pair<std::wstring, std::wstring> param(key, value);
+			paramsResult.insert(std::make_pair(key, value));
+		}
+
+		return zillians::language::stage::ErrorInfoContext(levelString, idString, paramsResult);
+	}
+
+private:
+	bool mAllMatch ;
+
+
+};
+
+} } } } // namespace zillians::language::tree::visitor
+
+#endif /* ZILLIANS_LANGUAGE_TREE_VISITOR_ERRORMESSAGEANNOTATIONCHECKVISITOR_H_ */
---
+++ test/testzillians-compiler/ThorScriptTreeTest/ErrorMessageAnnotationCheckVisitorTest/ErrorMessageAnnotationCheckVisitorTest.cpp
@@ -0,0 +1,188 @@
+/**
+ * Zillians MMO
+ * Copyright (C) 2007-2010 Zillians.com, Inc.
+ * For more information see http://www.zillians.com
+ *
+ * Zillians MMO is the library and runtime for massive multiplayer online game
+ * development in utility computing model, which runs as a service for every
+ * developer to build their virtual world running on our GPU-assisted machines.
+ *
+ * This is a close source library intended to be used solely within Zillians.com
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "core/Prerequisite.h"
+#include "language/tree/ASTNode.h"
+#include "language/tree/ASTNodeFactory.h"
+#include "language/tree/visitor/check/ErrorMessageAnnotationCheckVisitor.h"
+#include "../ASTNodeSamples.h"
+#include <iostream>
+#include <string>
+#include <limits>
+
+#define BOOST_TEST_MODULE ThorScriptTreeTest_ErrorMessageAnnotationCheckVisitorTest
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+using namespace zillians;
+using namespace zillians::language::tree;
+using namespace zillians::language::tree::visitor;
+
+BOOST_AUTO_TEST_SUITE( ThorScriptTreeTest_ErrorMessageAnnotationCheckVisitorTestSuite )
+
+ASTNode* createOKSample()
+{
+	Program* program = new Program();
+	{
+		Package* com_package = new Package(new SimpleIdentifier(L"com"));
+		program->root->addPackage(com_package);
+		{
+			Package* zillians_package = new Package(new SimpleIdentifier(L"zillians"));
+			com_package->addPackage(zillians_package);
+			{
+				ClassDecl* class_decl = new ClassDecl(new SimpleIdentifier(L"some_class"));
+				zillians_package->addObject(class_decl);
+				{
+					FunctionDecl* some_member_function = new FunctionDecl(
+							new SimpleIdentifier(L"some_member_function"),
+							NULL,
+							true,
+							Declaration::VisibilitySpecifier::PUBLIC,
+							Declaration::StorageSpecifier::NONE);
+					class_decl->addFunction(some_member_function);
+					{
+						Block* block = some_member_function->block;
+
+						{
+							DeclarativeStmt* stmt = new DeclarativeStmt(new VariableDecl(new SimpleIdentifier(L"a"), new TypeSpecifier(PrimitiveType::INT32), false, Declaration::VisibilitySpecifier::DEFAULT, Declaration::StorageSpecifier::NONE));
+							block->appendObject(stmt);
+						}
+
+						{
+							DeclarativeStmt* stmt = new DeclarativeStmt(new VariableDecl(new SimpleIdentifier(L"b"), new TypeSpecifier(PrimitiveType::INT32), false, Declaration::VisibilitySpecifier::DEFAULT, Declaration::StorageSpecifier::NONE));
+							block->appendObject(stmt);
+						}
+
+						{
+							// set error check annotation data
+							Annotation* msgParams = new Annotation(NULL);
+							msgParams->appendKeyValue(new SimpleIdentifier(L"id"), new StringLiteral(L"mCount"));
+							msgParams->appendKeyValue(new SimpleIdentifier(L"type"), new StringLiteral(L"int"));
+							Annotation* levelIdParams = new Annotation(NULL);
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"level"), new StringLiteral(L"warning"));
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"id"), new StringLiteral(L"undeclared_variable"));
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"parameters"), msgParams);
+
+							Annotation* anno = new Annotation(new SimpleIdentifier(L"static_test"));
+							anno->appendKeyValue(new SimpleIdentifier(L"expect_message"), levelIdParams);
+							Annotations* annos = new Annotations();
+							annos->appendAnnotation(anno);
+
+							// set error message context data
+							std::map<std::wstring, std::wstring> m;
+							m[L"id"] = L"mCount";
+							m[L"type"] = L"int";
+							auto errorContext = new zillians::language::stage::ErrorInfoContext(L"warning", L"undeclared_variable", m);
+
+							ExpressionStmt* stmt = new ExpressionStmt(new BinaryExpr(BinaryExpr::OpCode::ASSIGN, new PrimaryExpr(new SimpleIdentifier(L"undeclared_variable_name")), new PrimaryExpr(new SimpleIdentifier(L"b"))));
+							stmt->setAnnotation(annos);
+							stmt->set<zillians::language::stage::ErrorInfoContext>(errorContext);
+
+							block->appendObject(stmt);
+						}
+					}
+				}
+			}
+		}
+	}
+	return program;
+}
+
+ASTNode* createFailSample()
+{
+	Program* program = new Program();
+	{
+		Package* com_package = new Package(new SimpleIdentifier(L"com"));
+		program->root->addPackage(com_package);
+		{
+			Package* zillians_package = new Package(new SimpleIdentifier(L"zillians"));
+			com_package->addPackage(zillians_package);
+			{
+				ClassDecl* class_decl = new ClassDecl(new SimpleIdentifier(L"some_class"));
+				zillians_package->addObject(class_decl);
+				{
+					FunctionDecl* some_member_function = new FunctionDecl(
+							new SimpleIdentifier(L"some_member_function"),
+							NULL,
+							true,
+							Declaration::VisibilitySpecifier::PUBLIC,
+							Declaration::StorageSpecifier::NONE);
+					class_decl->addFunction(some_member_function);
+					{
+						Block* block = some_member_function->block;
+
+						{
+							DeclarativeStmt* stmt = new DeclarativeStmt(new VariableDecl(new SimpleIdentifier(L"a"), new TypeSpecifier(PrimitiveType::INT32), false, Declaration::VisibilitySpecifier::DEFAULT, Declaration::StorageSpecifier::NONE));
+							block->appendObject(stmt);
+						}
+
+						{
+							DeclarativeStmt* stmt = new DeclarativeStmt(new VariableDecl(new SimpleIdentifier(L"b"), new TypeSpecifier(PrimitiveType::INT32), false, Declaration::VisibilitySpecifier::DEFAULT, Declaration::StorageSpecifier::NONE));
+							block->appendObject(stmt);
+						}
+
+						{
+							// set error check annotation data
+							Annotation* msgParams = new Annotation(NULL);
+							msgParams->appendKeyValue(new SimpleIdentifier(L"id"), new StringLiteral(L"mCount"));
+							msgParams->appendKeyValue(new SimpleIdentifier(L"type"), new StringLiteral(L"int"));
+							Annotation* levelIdParams = new Annotation(NULL);
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"level"), new StringLiteral(L"warning"));
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"id"), new StringLiteral(L"undeclared_variable"));
+							levelIdParams->appendKeyValue(new SimpleIdentifier(L"parameters"), msgParams);
+
+							Annotation* anno = new Annotation(new SimpleIdentifier(L"static_test"));
+							anno->appendKeyValue(new SimpleIdentifier(L"expect_message"), levelIdParams);
+							Annotations* annos = new Annotations();
+							annos->appendAnnotation(anno);
+
+							// set error message context data
+							std::map<std::wstring, std::wstring> m;
+							m[L"id"] = L"mCount";
+							m[L"type"] = L"int";
+							m[L"extra_fail_key"] = L"extra_fail_value";
+							auto errorContext = new zillians::language::stage::ErrorInfoContext(L"warning", L"undeclared_variable", m);
+
+							ExpressionStmt* stmt = new ExpressionStmt(new BinaryExpr(BinaryExpr::OpCode::ASSIGN, new PrimaryExpr(new SimpleIdentifier(L"undeclared_variable_name")), new PrimaryExpr(new SimpleIdentifier(L"b"))));
+							stmt->setAnnotation(annos);
+							stmt->set<zillians::language::stage::ErrorInfoContext>(errorContext);
+
+							block->appendObject(stmt);
+						}
+					}
+				}
+			}
+		}
+	}
+	return program;
+}
+
+BOOST_AUTO_TEST_CASE( ThorScriptTreeTest_ErrorMessageAnnotationCheckVisitorTestCase1 )
+{
+	ErrorMessageAnnotationCheckVisitor checker;
+	ASTNode* okProgram = createOKSample();
+	checker.check(*okProgram);
+	BOOST_CHECK(checker.isAllMatch());
+
+	ASTNode* failProgram = createFailSample();
+	checker.check(*failProgram);
+	BOOST_CHECK(!checker.isAllMatch());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
