f229a7d29571bcb99570aecb30b32951ac09e467
Jerry <jerry@zillians.com>
2011-09-21 14:30:48 +0800

    make fail on purpose to test buildbot

--- include/language/grammar/ThorScript.h
+++ include/language/grammar/ThorScript.h
@@ -926,7 +926,7 @@ struct ThorScript : qi::grammar<Iterator, typename SA::start::attribute_type, de
 
 		start
 			= location [ typename SA::start::reset() ]
-				>> (program > qi::eoi)
+				>> (QWE program > qi::eoi)
 			;
 
 		/////////////////////////////////////////////////////////////////////
