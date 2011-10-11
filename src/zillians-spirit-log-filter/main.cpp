5953b43e47911071fe30a9a8f45d50682acb76ff
Jerry <jerry@zillians.com>
2011-10-11 15:53:47 +0800

    completely remove ticpp from spirit-log-filter

--- src/zillians-spirit-log-filter/main.cpp
+++ src/zillians-spirit-log-filter/main.cpp
@@ -3,14 +3,9 @@
 #include <iomanip>
 #include <ostream>
 
-#define TIXML_USE_TICPP
-#include <ticpp/ticpp.h>
-
 #define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
-
-#define ENABLE_BOOST_PROPERTY_TREE
 
 #define INDENT_CHAR  ' '
 #define INDENT_WIDTH 2
@@ -45,8 +40,6 @@ static std::basic_ostream<char> &_set_indent(std::basic_ostream<char> &ss, size_
 {
 	return ss << std::setfill(INDENT_CHAR) << std::setw(INDENT_WIDTH*depth) << "";
 }
-
-#ifdef ENABLE_BOOST_PROPERTY_TREE
 
 std::string parse_elem_bpp(ptree::value_type& elem, PARSE_STATE* parse_state, int depth)
 {
@@ -117,79 +110,6 @@ std::string parse_elem_bpp(ptree::value_type& elem, PARSE_STATE* parse_state, in
 	return ss.str();
 }
 
-#else
-
-std::string parse_elem_ticpp(ticpp::Element* elem, PARSE_STATE* parse_state, int depth)
-{
-	static std::string prev_elem_text;
-	std::stringstream ss;
-	std::string elem_value = elem->Value();
-	if(elem_value == "try")
-	{
-		*parse_state = STATE_TRY;
-		std::string raw_elem_text = elem->GetText(false);
-		std::string elem_text = raw_elem_text;
-		_filter_whitespace(elem_text);
-		if(!elem_text.empty() && elem_text != prev_elem_text)
-		{
-#ifdef NEW_STYLE_TRY_SUCCESS
-			_set_indent(ss, depth) << raw_elem_text << std::endl;
-			_set_indent(ss, depth) << '^' << std::string(DOT_WIDTH, DOT_CHAR) << " try" << std::endl;
-#else
-			_set_indent(ss, depth) << '<' << elem_value << '>' << raw_elem_text << "</" << elem_value << '>' << std::endl;
-#endif
-		}
-		prev_elem_text = elem_text;
-	}
-	else if(elem_value == "success")
-	{
-		*parse_state = STATE_SUCCESS;
-		std::string raw_elem_text = elem->GetText(false);
-		std::string elem_text = raw_elem_text;
-		_filter_whitespace(elem_text);
-		if(!elem_text.empty() && elem_text != prev_elem_text)
-		{
-#ifdef NEW_STYLE_TRY_SUCCESS
-			_set_indent(ss, depth) << raw_elem_text << std::endl;
-			_set_indent(ss, depth) << '^' << std::string(DOT_WIDTH, DOT_CHAR) << " success" << std::endl;
-#else
-			_set_indent(ss, depth) << '<' << elem_value << '>' << raw_elem_text << "</" << elem_value << '>' << std::endl;
-#endif
-		}
-		prev_elem_text = elem_text;
-	}
-	else if(elem_value == "fail")
-	{
-		*parse_state = STATE_FAIL;
-	}
-	else if(elem_value == "attributes")
-	{
-		*parse_state = STATE_ATTRIB;
-	}
-	else
-	{
-		if(elem->NoChildren())
-			_set_indent(ss, depth) << '<' << elem_value << "/>" << std::endl; // NOTE: never occurs due to "try/success/fail/attribute"
-		else
-		{
-			_set_indent(ss, depth) << '<' << elem_value << '>' << std::endl;
-			ticpp::Iterator<ticpp::Element> p; // NOTE: must declare on line separate from for loop
-			for(p = p.begin(elem); p != p.end(); p++)
-			{
-				PARSE_STATE parse_state = STATE_ANY;
-				std::string s = parse_elem_ticpp(p.Get(), &parse_state, depth+1);
-				if(parse_state == STATE_FAIL)
-					return "";
-				ss << s;
-			}
-			_set_indent(ss, depth) << "</" << elem_value << '>' << std::endl;
-		}
-	}
-	return ss.str();
-}
-
-#endif
-
 int main(int argc, char** argv)
 {
 	if(argc != 2)
@@ -198,23 +118,17 @@ int main(int argc, char** argv)
 	try
 	{
 		PARSE_STATE parse_state = STATE_ANY;
-#ifdef ENABLE_BOOST_PROPERTY_TREE
 	    ptree pt;
 	    read_xml(pFilename, pt);
 		std::string s = parse_elem_bpp(*pt.begin(), &parse_state, 0);
-#else
-		ticpp::Document doc(pFilename);
-		doc.LoadFile();
-		std::string s = parse_elem_ticpp(dynamic_cast<ticpp::Node*>(&doc)->FirstChildElement(), &parse_state, 0);
-#endif
 		_replace_string(s, "\\n", "");
 		if(parse_state == STATE_FAIL)
 			return -1;
 		std::cout << s;
 	}
-	catch(ticpp::Exception& ex)
+	catch(...)
 	{
-		std::cout << ex.what();
+		std::cout << "fail!" << std::endl;
 		return -1;
 	}
 
