ea9efb4fb048a043dd63c2d1d24da1ebaa54231c
Jerry <jerry@zillians.com>
2011-11-04 16:29:56 +0800

    fix bugs in stub test

--- test/testzillians-compiler/ThorScriptStubTest/test.sh
+++ test/testzillians-compiler/ThorScriptStubTest/test.sh
@@ -2,14 +2,19 @@
 
 TEMP_FILE_A=`mktemp`
 TEMP_FILE_B=`mktemp`
+TEMP_FILE_C=`mktemp`
 
-TSCOMPILE=$1
-INPUT=$2
-GOLD_OUTPUT=$3
+TS_COMPILE=$1
+TS_STUB=$2
+INPUT=$3
+STUB_TYPE=$4
+GOLD_OUTPUT=$5
 
-$TSCOMPILE $INPUT --root-dir=`dirname ${INPUT}` --emit-ast=$TEMP_FILE_A
+$TS_COMPILE $INPUT --root-dir=`dirname $INPUT` --emit-ast=$TEMP_FILE_A
+$TS_STUB --stub-type=$STUB_TYPE --input=$TEMP_FILE_A --output=$TEMP_FILE_B
 
-diff $TEMP_FILE_A $TEMP_FILE_B
+cat $GOLD_OUTPUT > $TEMP_FILE_C
+diff $TEMP_FILE_B $TEMP_FILE_C
 ERROR_CODE="$?"
 
 if [ $ERROR_CODE -ne 0 ];
