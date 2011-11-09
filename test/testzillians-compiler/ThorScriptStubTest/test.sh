513af569215c8fbabee8a578285a46b54041142c
Jerry <jerry@zillians.com>
2011-11-09 18:08:47 +0800

    waypoint ckin for thor stub

--- test/testzillians-compiler/ThorScriptStubTest/test.sh
+++ test/testzillians-compiler/ThorScriptStubTest/test.sh
@@ -7,14 +7,20 @@ TEMP_FILE_C=`mktemp`
 TS_COMPILE=$1
 TS_STUB=$2
 INPUT=$3
-STUB_TYPE=$4
-GOLD_OUTPUT=$5
-FORMAT_SH=$6
+ROOT_DIR=$4
+STUB_TYPE=$5
+GOLD_OUTPUT=$6
+FORMAT_SH=$7
 
 TEMP_FILE_A="${TEMP_FILE_A}.ast"
-$TS_COMPILE $INPUT --root-dir=`dirname $INPUT` --emit-ast=$TEMP_FILE_A
+$TS_COMPILE $INPUT --root-dir=$ROOT_DIR --emit-ast=$TEMP_FILE_A
 $TS_STUB $TEMP_FILE_A --stub-type=$STUB_TYPE --game-name=DummyGameName --translator-uuid=DummyTranslatorUUID --module-uuid=DummyModuleUUID --stdout > $TEMP_FILE_B
 $FORMAT_SH $TEMP_FILE_B
+
+printf "temp file: "
+printf $TEMP_FILE_B
+printf " --> for stub type: "
+echo $STUB_TYPE
 
 cat $GOLD_OUTPUT > $TEMP_FILE_C
 diff $TEMP_FILE_B $TEMP_FILE_C
