c1b2daf1cb837254dc38beba99c199e1bf25a6ef
Jerry <jerry@zillians.com>
2011-11-25 16:35:04 +0800

    fix TemplatedIdentifier grammar bug
    * fix grammar/actions/semantic-check
    * disable ALL variadic arg tests

--- include/language/action/basic/SpecifierActions.h
+++ include/language/action/basic/SpecifierActions.h
@@ -85,13 +85,19 @@ struct thor_type
 		BIND_CACHED_LOCATION(_result = new TypeSpecifier(function_type));
 	}
 	END_ACTION
+};
 
-	BEGIN_ACTION(init_ellipsis)
+struct thor_param_type
+{
+	DEFINE_ATTRIBUTES(TypeSpecifier*)
+	DEFINE_LOCALS(LOCATION_TYPE)
+
+	BEGIN_ACTION(init_type)
 	{
 #ifdef DEBUG
-		printf("thor_type::init_ellipsis param(0) type = %s\n", typeid(_param_t(0)).name());
+		printf("thor_param_type::init_type param(0) type = %s\n", typeid(_param_t(0)).name());
 #endif
-		BIND_CACHED_LOCATION(_result = new TypeSpecifier(PrimitiveType::VARIADIC_ELLIPSIS));
+		BIND_CACHED_LOCATION(_result = new TypeSpecifier(_param(0)));
 	}
 	END_ACTION
 };
@@ -142,14 +148,14 @@ struct annotation
 		printf("annotation::init param(0) type = %s\n", typeid(_param_t(0)).name());
 		printf("annotation::init param(1) type = %s\n", typeid(_param_t(1)).name());
 #endif
-		SimpleIdentifier* name = _param(0);
+		SimpleIdentifier* ident = _param(0);
 		if(_param(1).is_initialized())
 		{
 			_result = *_param(1);
-			_result->name = name;
+			_result->name = ident;
 		}
 		else
-			BIND_CACHED_LOCATION(_result = new Annotation(name));
+			BIND_CACHED_LOCATION(_result = new Annotation(ident));
 	}
 	END_ACTION
 };
